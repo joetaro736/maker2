42559c1be7d62046362570127577d03cf8884e7d
joetaro736 <joequegyra@gmail.com>
2025-08-16 13:23:22 -0300

    Add files via upload

--- 1_Impactos.py
+++ 1_Impactos.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title='Impactos', page_icon='', layout='wide')
 st.sidebar.header('Joe, Gustavo, Nicollas, Vinicius e Guilherme')
 
 st.sidebar.title("""Ciberbullying
