c5aa4559a0ec5ef404c876313243ead72cf04e0d
joetaro736 <joequegyra@gmail.com>
2025-08-16 17:47:15 -0300

    Update 1_Impactos.py

--- 1_Impactos.py
+++ 1_Impactos.py
@@ -1,6 +1,1 @@
-import streamlit as st
-st.sidebar.header('Joe, Gustavo, Nicollas, Vinicius e Guilherme')
 
-st.sidebar.title("""Ciberbullying
-                 6°B""")
-st.sidebar.image('download.png')
